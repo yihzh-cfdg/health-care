ee9491824529120b0c2490ae53736486248ca1fa
yihzh-cfdg <1095061578@qq.com>
2024-02-13 17:30:26 +0800

    backend addon

---
+++ backend/src/test/java/com/hc/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hc.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
